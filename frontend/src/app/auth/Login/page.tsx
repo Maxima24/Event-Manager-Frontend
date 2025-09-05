e262b440bb259a8c62e1d5613290ec360a4433e2
SHIGERU KAMADO <olomolapraiseoluwadamilare@gmail.com>
2025-09-05 09:11:38 +0100

    G1-16 designed login page

---
+++ frontend/src/app/auth/Login/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Login:", { email, password });
+  };
+
+  return (
+    <div
+      className=" h-80 font-sans flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/bg.png')" }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 mt-20 rounded-lg shadow-lg w-140 h-150 mb-20"
+      >
+        <h2 className="text-5xl text-purple-950 font-bold mt-0 mb-2 text-center">
+          Login
+        </h2>
+
+        <div className="flex justify-center items-center gap-2 mb-2 mt-3">
+          <img src="/imageLogo.png" width={50} height={50} alt="Logo" />
+          <h1 className="text-2xl text-purple-950 font-bold">EventHive</h1>
+        </div>
+
+        <h2 className="font-bold text-3xl mb-6 text-center">Welcome Back!</h2>
+
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full text-xs h-8 p-3 border border-gray-300 rounded-md mb-4"
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full text-xs h-8 p-3 border border-gray-300 rounded-md mb-4"
+        />
+
+        <div className="flex justify-between items-center mb-4">
+          <label className="flex items-center text-xs">
+            <input
+              type="checkbox"
+              className="w-3 h-3 mr-1 accent-blue-600"
+            />
+            Remember Me
+          </label>
+
+          <Link
+            href="/forgot"
+            className="text-blue-600 hover:text-blue-900 hover:underline text-xs"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full text-xs h-8 font-bold bg-purple-800 text-white py-2 rounded hover:bg-purple-950 transition"
+        >
+          Submit
+        </button><br />
+
+        <p className="text-xs pt-2">Haven't registered?<Link href='/Register' className=" text-blue-600 hover:text-blue-900 hover:underline"> Click here.</Link></p>
+
+
+        <div className="flex items-center gap-2 mt-4">
+             <hr className="flex-grow border-gray-300" />
+           <span className="text-xs text-gray-500">Or log in with</span>
+           <hr className="flex-grow border-gray-300" />
+        </div>
+        <p 
+        className="text-xs pt-2 mt-3">
+            By clicking on Login or  Apple, Google, or Facebook icons,
+             you agree to EventHire’s 
+             <Link href='/' 
+             className="text-blue-600 hover:text-blue-900 hover:underline"> Terms of Service</Link> and <Link href='/' className ="text-blue-600 hover:text-blue-900  hover:underline">Privacy Policy</Link>.
+        </p>
+
+        <div className="flex items-center justify-center gap-20 mt-5">
+            <button className="p-2 border rounded hover:bg-gray-100">
+                <img src="/apple.png" alt="Apple" className="w-6 h-6" />
+            </button>
+            <button className="p-2 border rounded hover:bg-gray-100">
+                <img src="/google.png" alt="Google" className="w-6 h-6" />
+            </button>
+            <button className="p-2 border rounded hover:bg-gray-100">
+                <img src="/facebook.png" alt="Facebook" className="w-6 h-6" />
+            </button>
+        </div>
+        <p className="text-blue-500 text-xs mt-6 justify-center flex">Need help finding your tickets?</p>
+      </form>
+    </div>
+  );
+}
